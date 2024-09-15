56fc86ec65f4bf92ad42314b6a98949105c7984a
zafariak <zafariqbal.akhtar@dataport.de>
2024-09-15 17:02:29 +0200

    update AccountControllerTest

--- account/webservice/src/test/java/vault/webservice/account/AccountControllerTest.java
+++ account/webservice/src/test/java/vault/webservice/account/AccountControllerTest.java
@@ -5,8 +5,6 @@ import static org.mockito.Mockito.when;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
