68f316abc6a7806b438f9a25c5c5ae94b81834a9
zafarius <zafariqbal.akhtar@dataport.de>
2024-09-20 18:44:32 +0200

    remove unnecessary annotation

--- file/webservice/src/main/java/vault/webservice/file/FileControllerConfiguration.java
+++ file/webservice/src/main/java/vault/webservice/file/FileControllerConfiguration.java
@@ -2,11 +2,9 @@ package vault.webservice.file;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import vault.security.SecurityConfiguration;
 
 @Configuration
-@EnableMethodSecurity(securedEnabled = true)
 @ComponentScan(basePackageClasses = {
         FileControllerConfiguration.class,
         SecurityConfiguration.class
